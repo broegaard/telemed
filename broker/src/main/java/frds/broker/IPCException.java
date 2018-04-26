2586e0e7ebd30c0014aa1b2b7f5085cbc8945cab
HenrikBaerbak <hbc@cs.au.dk>
2018-04-26 12:44:50 +0200

    Fix #3. Added constructor for IPCException that allows http status codes to be transferred as part of cross network exception handling

--- broker/src/main/java/frds/broker/IPCException.java
+++ broker/src/main/java/frds/broker/IPCException.java
@@ -20,15 +20,40 @@ package frds.broker;
 
 /** The superclass of any exceptions occurring during IPC between
  * client and server.
+ * <p>
+ *   To facilitate communication about root causes, http
+ *   status codes can be used using one of the constructors.
+ * </p>
  */
 public class IPCException extends RuntimeException {
 
-  public IPCException(String message, Throwable exception) {
-    super(message, exception);
+  private static final int NOT_DEFINED = -1;
+  private int statusCode;
+
+  public IPCException(int httpStatusCode, String message) {
+    super(message);
+    statusCode = httpStatusCode;
   }
 
   public IPCException(String message) {
     super(message);
+    statusCode = NOT_DEFINED;
+  }
+
+  public IPCException(String message, Throwable exception) {
+    super(message, exception);
+    statusCode = NOT_DEFINED;
+  }
+
+  /** Get the HTTP Status code associated with this
+   * exception. In case the failure originated on the
+   * server side this may provide further qualification
+   * of the type of error. If it has the value NOT_DEFINED
+   * then this information is not available.
+   * @return HTTP status code or NOT_DEFINED.
+   */
+  public int getStatusCode() {
+    return statusCode;
   }
 
   private static final long serialVersionUID = 7220436156585793897L;
